4166a98aece19db023fc05ed6f16584f346e8b4d
Yevhenii2022 <3starosta3@gmail.com>
2023-09-07 15:10:36 +0300

    add default project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			screens: {
+				mob: "320px",
+				tab: "768px",
+				desk: "1280px",
+			},
+
+			// backgroundImage: {
+			//   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+			//   'gradient-conic':
+			//     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			// },
+		},
+	},
+	plugins: [],
+};
+export default config;
